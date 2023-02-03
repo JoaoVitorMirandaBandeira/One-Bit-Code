7808cc72073e4677c00629668dd93f89a94a5b09
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-02-02 23:33:14 -0300

    Armazenando dados no navegador

---
+++ JavaScript-DOM/Exercicio/ArmazenamentoNavegador/index.js
@@ -0,0 +1,16 @@
+/*Session storage 
+    Armazena dados durante a navegação
+    Persite mesmo após mudar de pagina na mesma aba
+    Ao fechar a aba do site ele e limpo */
+
+/*Local storage
+    Dados persitem mesmo apos fechar a aba ou janela 
+    Existem para sempre ate serem apagados
+ */
+
+/*Cookies
+    Dados armazenados em pequenos arquivos de texto no computador do cliente 
+    Podem possuir data de expiração
+    Forma mais tradicional de "Lembrar" de informações 
+    São enviados na requisição e podem existir no servidor, sendo devolvidos quando uma pagina HTML for solicitada
+ */
