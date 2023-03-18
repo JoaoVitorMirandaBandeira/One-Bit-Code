4a86f4cf723398fc4a6e4070b79affd8023793c5
joao vitor <joaovitormirandabandeira@gmail.com>
2023-03-17 23:54:58 -0300

    Estilizando a pagina,e incluindo os metodos que consomen a api

--- JavaScript-Avançado/Requisicao-GET/index.js
+++ JavaScript-Avançado/Requisicao-GET/index.js
@@ -1,5 +1,24 @@
-async function requestsGET(){
-    const response = await fetch('https://restcountries.com/v3.1/all')
-    const countre = await response.json()
+async function requestsGET() {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const country = await response.json();
+
+  country.forEach(formatCauntry);
 }
-requestsGET()
+
+async function formatCauntry(country) {
+  const div = document.createElement("div");
+  div.classList.add("country");
+
+  const h2 = document.createElement("h2");
+  const nameCountry = country.name.common;
+  h2.innerText = nameCountry;
+  console.log(nameCountry);
+
+  const img = document.createElement("img");
+  img.src = country.flags.svg;
+  img.alt = nameCountry;
+
+  div.append(h2, img);
+  document.querySelector("#countainer").append(div);
+}
+requestsGET();
