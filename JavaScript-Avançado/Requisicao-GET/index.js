b06675ab7176b1b3665e9f44a722e36e841c270d
joao vitor <joaovitormirandabandeira@gmail.com>
2023-03-18 00:15:12 -0300

    Implementando um filtro de paises, e performando o codigo

--- JavaScript-Avançado/Requisicao-GET/index.js
+++ JavaScript-Avançado/Requisicao-GET/index.js
@@ -1,24 +1,60 @@
+// async function requestsGET() {
+//   const response = await fetch("https://restcountries.com/v3.1/all");
+//   const country = await response.json();
+
+//   country.forEach(formatCauntry);
+// }
+
+// async function formatCauntry(country) {
+//   const div = document.createElement("div");
+//   div.classList.add("country");
+
+//   const h2 = document.createElement("h2");
+//   const nameCountry = country.name.common;
+//   h2.innerText = nameCountry;
+//   console.log(nameCountry);
+
+//   const img = document.createElement("img");
+//   img.src = country.flags.svg;
+//   img.alt = nameCountry;
+
+//   div.append(h2, img);
+//   document.querySelector("#countainer").append(div);
+// }
+// requestsGET();
+
 async function requestsGET() {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const country = await response.json();
-
-  country.forEach(formatCauntry);
+  const paises = [
+    "Brazil",
+    "Spain",
+    "United States Minor Outlying Islands",
+    "South Africa",
+  ];
+  let result = [];
+  country.find((e) => {
+    paises.includes(e.name.common) ? result.push(e) : false;
+  });
+  formatCauntry(result);
 }
 
-async function formatCauntry(country) {
-  const div = document.createElement("div");
-  div.classList.add("country");
+async function formatCauntry(element) {
+  element.forEach((country) => {
+    const div = document.createElement("div");
+    div.classList.add("country");
 
-  const h2 = document.createElement("h2");
-  const nameCountry = country.name.common;
-  h2.innerText = nameCountry;
-  console.log(nameCountry);
+    const h2 = document.createElement("h2");
+    const nameCountry = country.name.common;
+    h2.innerText = nameCountry;
+    console.log(nameCountry);
 
-  const img = document.createElement("img");
-  img.src = country.flags.svg;
-  img.alt = nameCountry;
+    const img = document.createElement("img");
+    img.src = country.flags.svg;
+    img.alt = nameCountry;
 
-  div.append(h2, img);
-  document.querySelector("#countainer").append(div);
+    div.append(h2, img);
+    document.querySelector("#countainer").append(div);
+  });
 }
 requestsGET();
