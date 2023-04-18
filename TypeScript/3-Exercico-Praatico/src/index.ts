048affc77b70b19cf14d878e07c072e48f39da88
joao vitor <joaovitormirandabandeira@gmail.com>
2023-04-17 22:24:00 -0300

    Criando metodo de adicionar tripulantes a nave

--- TypeScript/3-Exercico-Praatico/src/index.ts
+++ TypeScript/3-Exercico-Praatico/src/index.ts
@@ -4,9 +4,32 @@ function creteSpaceShip(name:string, pilot:string, crewLimit: number){
     const spaceShip = {
         name,
         pilot,
+        crewLimit,
         crew : [crewLimit],
         inMission : false
     }
     spaceShips.push(spaceShip)
-    console.log(`A nave espacial ${spaceShip.name} foi adicionada com sucesso...`)
+    alert(`A nave espacial ${spaceShip.name} foi adicionada com sucesso...`)
+}
+
+function addCrewSpaceShip(nameSpaceShip: string, name: string){
+    const spaceShip = findSpaceShip(nameSpaceShip)
+    if(spaceShip.crewLimit > spaceShip.crew.length){
+        spaceShip.crew.push(name)
+        alert(`${name} foi adiconado a tripulação com sucesso...`)
+    }else{
+        alert(`A tripulação esta completa, não foi possivel adicionar ${name} a tripulação...`)
+    }
+}
+
+function findSpaceShip(name: string){
+    let spaceship: {
+        name: string,
+        pilot: string,
+        crewLimit:number,
+        crew: string[],
+        inMission: boolean
+      }
+    spaceship = spaceShips.find((e) => { e.name === name})
+    return spaceship
 }
