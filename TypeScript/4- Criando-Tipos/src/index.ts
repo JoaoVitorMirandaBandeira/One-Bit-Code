d2df2aa12f46fff90caa0d27ac5a34cfdeccabff
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-04-28 20:56:50 -0300

    Criando metodos de adicionar, buscar e alterar status de um planeta

--- TypeScript/4- Criando-Tipos/src/index.ts
+++ TypeScript/4- Criando-Tipos/src/index.ts
@@ -6,10 +6,30 @@ type Planet = {
     situation: PlanetSituation,
     satellites: string[]
 }
-
 const planets: Planet[] = []
 
-const addPlanet = () => {
+const addPlanet = (name: string, codernadas: PlanetLocation,situation: PlanetSituation) => {
+    planets.push({
+        name: name,
+        location: codernadas,
+        situation: situation,
+        satellites: []
+    })
+    alert(`Planet ${name} successfully added ...`)
+}
 
-    
+const updateStatusPlanet = (name: string, situation: PlanetSituation) => {
+    const planet:Planet = searchPlanet(name)
+    if(planet){
+        planet.situation = situation
+        alert(`Status of planet ${name} successfully changed to ${situation} ...`)
+    }else{
+        alert(`Planet ${name} not found ...`)
     }
+}
+
+const searchPlanet = (name: string) => {
+    let planet: Planet
+    planet = planets.find((e) => {return e.name === name})
+    return planet
+}
