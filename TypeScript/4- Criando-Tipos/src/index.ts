6b2c82e7befb666f2e028d17fa888806b02bfc00
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-05-01 11:12:13 -0300

    Finalizando metodas de exibir e remover satelites eplanetas

--- TypeScript/4- Criando-Tipos/src/index.ts
+++ TypeScript/4- Criando-Tipos/src/index.ts
@@ -42,3 +42,38 @@ const addSatellitesPlanet = (name: string, nameSatellites: string) =>{
         alert(`Planet ${name} not found ...`)
     }
 }
+
+const removeSatellitesPlanet = (name: string, nameSatellites: string) => {
+    const planet = searchPlanet(name)
+    if (planet) {
+        const indexSatellite = Number( planet.satellites.find((e, index) => {if(e === nameSatellites){
+            return index
+        }}))
+        if(indexSatellite){
+            planet.satellites.splice(indexSatellite, 1)
+        }
+    }
+}
+
+const listPlanet = () => {
+    let text: string = ""
+    planets.forEach( (e) => {
+        text +=`Name : ${e.name}
+        Situatuon : ${e.situation}
+        Location : ${locationPlanet(e)}
+        Satellites : ${listSatelite(e)}\n`
+    })
+    alert(text)
+}
+
+const locationPlanet = (planet: Planet) => {
+    let location: string = ""
+    planet.location.forEach( e => { location += e})
+    return location
+}
+
+const listSatelite = (planet: Planet) => {
+    let satellites: string = ""
+    planet.satellites.forEach( e => { satellites += e})
+    return satellites
+}
