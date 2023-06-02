08ff6a4f83f3779d91ce569c2a6510e098d6e0a0
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-06-02 10:52:20 -0300

    atualização

--- MongoDB/3-EJSComExpress/app.js
+++ MongoDB/3-EJSComExpress/app.js
@@ -1,17 +1,20 @@
 const express = require('express')
-const checkListRoutes = require('./src/routes/checklist')
+const path = require('path')
+
+const checkListRouter = require('./src/routes/checklist')
+const rootRouter = require('./src/routes/index')
 
 require('./config/database')
 
 const app = express();
 app.use(express.json())
 
-// Primeira Rota
-app.get('/', (req, res) => {
-    res.send('<h1>Minha Lista de Tarefas</h1>');
-});
+app.set('views',path.join(__dirname,'src/views'))
+app.set('view engine','ejs')
 
-app.use('/checklist', checkListRoutes)
+app.use('/', rootRouter)
+app.use('/checklist', checkListRouter)
+
 
 // Responsável por iniciar o server
 app.listen(3000, () => {
